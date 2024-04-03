82495f9665aced9470fcf4844a29973dfe387799
ku9999 <155805991+ku9999@users.noreply.github.com>
2024-04-03 18:22:57 +0600

    Add files via upload

---
+++ текстовое увидомление .py
@@ -0,0 +1,8 @@
+﻿#чтобы заработало pip install plyer
+
+from plyer import notification
+notification.notify(
+    title='Ожидается кратко временный дожди',
+    message='Какие там дожди апокалипсис ёбаный в рот',
+    timeout=10
+)
